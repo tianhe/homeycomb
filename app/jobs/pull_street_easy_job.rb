96a3021b66cbb189e90609cd92260d08a174cdbc
Tian He <tianhe@gmail.com>
2015-05-25 21:38:42 -0400

    update job to fix neighborhood param

--- app/jobs/pull_street_easy_job.rb
+++ app/jobs/pull_street_easy_job.rb
@@ -2,7 +2,7 @@ class PullStreetEasyJob < ActiveJob::Base
   queue_as :default
 
   def perform(neighborhoods, offset=0)
-    street_easy_listings = StreetEasyHelper.get_latest_listings(query_neighborhoods, offset)
+    street_easy_listings = StreetEasyHelper.get_latest_listings(neighborhoods, offset)
     Listing.import_from_street_easy(street_easy_listings.listings)
   end
 end
